451fe0772659006d09df8d87265cc744d377b3da
peterrobert <pwambui93@gmail.com>
2020-09-19 12:29:41 +0300

    added a column to posts table

---
+++ db/migrate/20200919092555_add_category_id_column.rb
@@ -0,0 +1,5 @@
+class AddCategoryIdColumn < ActiveRecord::Migration[6.0]
+  def change
+   add_column :posts, :category_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_19_092139) do
+ActiveRecord::Schema.define(version: 2020_09_19_092555) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2020_09_19_092139) do
     t.text "interior_features"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "category_id"
   end
 
 end
