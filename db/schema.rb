3a3af60a4043c97613f9ddbb157657ad6f09239e
peterrobert <pwambui93@gmail.com>
2020-09-19 12:24:16 +0300

    created the relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_19_083854) do
+ActiveRecord::Schema.define(version: 2020_09_19_092139) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
