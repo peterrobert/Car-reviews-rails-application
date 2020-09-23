6a5ee911080514c7eec9e67fe2324ef720fe7dd8
peterrobert <pwambui93@gmail.com>
2020-09-23 14:25:15 +0300

    added the navigation

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,23 @@
 const { environment } = require('@rails/webpacker')
 
 const webpack = require('webpack')
+
+const { environment } = require('@rails/webpacker')
+
+const { environment } = require('@rails/webpacker')
+
+const webpack = require("webpack")
+
+environment.plugins.append("Provide", new webpack.ProvidePlugin({
+
+$: 'jquery',
+
+jQuery: 'jquery',
+
+Popper: ['popper.js', 'default']
+
+}))
+
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
     $: 'jquery',
