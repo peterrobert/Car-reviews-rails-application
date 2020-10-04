b6bc1a633cc1388ad4a0971fb36a91426222f000
peterrobert <pwambui93@gmail.com>
2020-10-04 14:46:00 +0300

    created the posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
     def show
         @post = Post.find(params[:id])
-        @post_avators = @post.image_thumbnails.find_by(params[:id])
+        @post_avators = ImageThumbnail.where(post_id == params[:id])
     end
 end
