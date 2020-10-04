72a4db962c1ef4eec888ae604e584cf86c226b82
peterrobert <pwambui93@gmail.com>
2020-10-04 15:17:29 +0300

    creatd the caurosel images

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
     def show
         @post = Post.find(params[:id])
-        @post_avators = ImageThumbnail.where(post_id == params[:id])
+        @post_avators = ImageThumbnail.where(:post_id == params[:id])
     end
 end
