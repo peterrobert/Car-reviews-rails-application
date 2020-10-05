e361783acdf43ea57f8b88638a274c89411852a4
peterrobert <pwambui93@gmail.com>
2020-10-05 11:56:26 +0300

    created the show html

--- app/controllers/categorys_controller.rb
+++ app/controllers/categorys_controller.rb
@@ -1,5 +1,6 @@
 class CategorysController < ApplicationController
     def show
-        
+        @category = Category.find(params[:id])
+        @category_show = @category.posts.where(:category_id == params[:id])
     end
 end
